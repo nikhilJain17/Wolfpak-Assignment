ecd168b241d2023d3ff0278cca2effe5671a0374
Test <nikhil.jain0987@gmail.com>
2015-06-15 23:13:38 -0400

    Touch listener for camera added

--- Wolfpak-Camera/app/src/main/java/com/hello/learning/wolfpak_camera/TakePictureActivity.java
+++ Wolfpak-Camera/app/src/main/java/com/hello/learning/wolfpak_camera/TakePictureActivity.java
@@ -5,28 +5,41 @@ import java.io.IOException;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Point;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.Bundle;
 import android.service.notification.StatusBarNotification;
+import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class TakePictureActivity extends Activity {
+public class TakePictureActivity extends Activity implements View.OnTouchListener {
 
     private Camera mCamera;
     private CameraView mView;
-    Parameters parameters; // specify size of preview (prevents stretching)
 
+    SurfaceView surfaceView;
+    RelativeLayout layout;
+    Button takePicture;
+
+    Canvas canvas;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         // fullscreen code
 
@@ -39,11 +52,46 @@ public class TakePictureActivity extends Activity {
 
         // end fullscreen code
 
+        takePicture = new Button(this);
+
         mCamera = Camera.open();
-        mView = new CameraView(this);
 
 
-        setContentView(mView);
+     // add button code
+        // init all views to be added
+
+        surfaceView = new CameraView(this);
+        surfaceView.setOnTouchListener(this);
+
+         layout = (RelativeLayout) findViewById(R.id.layout); // master layout to hold all views
+
+        if (layout != null) {
+            Log.d("Layout: ", "Not null");
+        }
+
+        takePicture = (Button) findViewById(R.id.takePictureButton);
+
+
+        // add the views and set their parameters
+            // basically, instead of instantiating it in xml, we are doing it programmatically (like swing)
+
+        //layout.addView(mView);
+        //layout.addView(takePicture);
+
+
+        setContentView(surfaceView);
+        //setContentView(R.layout.activity_take_picture);
+        //setContentView(layout);
+    }
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        Intent intent = new Intent(this, EditPicture.class);
+        startActivity(intent);
+
+        return false;
     }
 
     // extending SurfaceView to render the camera images
@@ -69,14 +117,16 @@ public class TakePictureActivity extends Activity {
         public void surfaceCreated(SurfaceHolder holder) {
 
             try {
-
                 mCamera.setDisplayOrientation(90); // set proper orientation (got rid of stretching)
                 mCamera.setPreviewDisplay(mHolder);
+
 
             } catch (IOException e) {
                 mCamera.release();
             }
+
             mCamera.startPreview();
+
         }
 
         @Override
