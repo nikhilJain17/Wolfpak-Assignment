ded8c4179d98eb955e69f34b1d655cda4ec837c5
Test <nikhil.jain0987@gmail.com>
2015-06-21 11:53:56 -0400

    Added color choosing for ColorPickerFragment

--- Wolfpak-Camera/app/src/main/java/com/hello/learning/wolfpak_camera/ColorPickerFragment.java
+++ Wolfpak-Camera/app/src/main/java/com/hello/learning/wolfpak_camera/ColorPickerFragment.java
@@ -3,6 +3,7 @@ package com.hello.learning.wolfpak_camera;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.SeekBar;
@@ -21,13 +22,61 @@ public class ColorPickerFragment extends DialogFragment {
     private SeekBar blueSeekBar;
     private View colorView;
     private int color;
+    private View colorDialogView; // holds the layout of the DialogFragment
 
     @Override
     public Dialog onCreateDialog (Bundle bundle) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Choose color");
+        builder.setCancelable(true);
+        colorDialogView = getActivity().getLayoutInflater().inflate(R.layout.fragment_color, null);
+        builder.setView(colorDialogView);
+
+        alphaSeekBar = (SeekBar) colorDialogView.findViewById(R.id.alphaSeekBar);
+        redSeekBar = (SeekBar) colorDialogView.findViewById(R.id.redSeekBar);
+        greenSeekBar = (SeekBar) colorDialogView.findViewById(R.id.greenSeekBar);
+        blueSeekBar = (SeekBar) colorDialogView.findViewById(R.id.blueSeekBar);
+
+        // attach listeners
+        alphaSeekBar.setOnSeekBarChangeListener(colorChangedListener);
+        redSeekBar.setOnSeekBarChangeListener(colorChangedListener);
+        blueSeekBar.setOnSeekBarChangeListener(colorChangedListener);
+        greenSeekBar.setOnSeekBarChangeListener(colorChangedListener);
+
+        return builder.create();
 
     }
+
+    // SeekBar listener
+    private SeekBar.OnSeekBarChangeListener colorChangedListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            // user, not system, changed seekbar values
+            if (fromUser) {
+
+                // define the ARGB value for the color from the values of the seekbar
+                // note that the seekbar goes from 0 to 254
+                color = Color.argb(alphaSeekBar.getProgress(), redSeekBar.getProgress(),
+                        greenSeekBar.getProgress(), blueSeekBar.getProgress());
+
+                colorDialogView.setBackgroundColor(color);
+            }
+
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
 
 
 
